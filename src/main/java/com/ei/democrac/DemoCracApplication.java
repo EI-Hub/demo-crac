ac65cea98fe6e024b2eb483093b9601d42538a20
Binh Le <lxhbinh@gmail.com>
2023-09-14 13:26:32 +0200

    Init

---
+++ src/main/java/com/ei/democrac/DemoCracApplication.java
@@ -0,0 +1,13 @@
+package com.ei.democrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ei/democrac/DemoCracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ei.democrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
