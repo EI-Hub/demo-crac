9991280040835b2f096a383145fa2ae096e095d5
Binh Le <lxhbinh@gmail.com>
2023-09-16 09:00:32 +0200

    Add tomcat specially built for CRaC

---
+++ src/main/java/com/ei/democrac/TestController.java
@@ -0,0 +1,14 @@
+package com.ei.democrac;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
